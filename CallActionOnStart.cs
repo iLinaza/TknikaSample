4bdb03773aca4f11315ba5da87940185f9fb2d3c
AlbertoCarlier <acarlier@virtualwareco.com>
2023-05-23 09:32:57 +0200

    Completo el script

---
+++ CallActionOnStart.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Viroo.Interaction;
+
+namespace VirooLab.Examples
+{
+    public class CallActionOnStart : MonoBehaviour
+    {
+        [SerializeField]
+        private List<ObjectAction> actions = default;
+
+
+        private void Start()
+        {
+            foreach (var action in actions)
+            {
+                action.LocalExecute(string.Empty);
+            }
+        }
+    }
+}
